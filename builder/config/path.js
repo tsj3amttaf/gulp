9378c1e830bfaac64c261dae9f2524f2578d83c1
tsj3amttaf <uyaskjahhkaskdaodpqwodhjkbz@gmail.com>
2024-10-05 21:57:01 +0700

    Загружаю первые документы в GitHib

---
+++ builder/config/path.js
@@ -0,0 +1,24 @@
+
+// Получаем имя папки проекта
+import * as nodePath from "path";
+const rootFolder = nodePath.basename( nodePath.resolve() );
+
+// Папка готовой сборки проекта
+const buildFolder = "./dist";
+
+// Папка с исходными файлами проекта
+const srcFolder   = "./src";
+
+const path = {
+    build: {
+        files: "${srcFolder}/files/"
+    },
+    src: {
+        files: "${srcFolder}/files/**/*.*"
+    },
+    watch: {},
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder
+}
