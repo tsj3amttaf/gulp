125993f29cdd2cbdd594b6f0d599d57fcb869ece
tsj3amttaf <uyaskjahhkaskdaodpqwodhjkbz@gmail.com>
2024-10-21 17:41:59 +0700

    save

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,5 +1,6 @@
-// Подключаю плагин
+// Подключаю плагины
 import fileInclude from 'gulp-file-include';
+import webpHtmlNosvg from 'gulp-webp-html-nosvg';
 
 export const html = () => {
 
@@ -8,6 +9,12 @@ export const html = () => {
 
     // Собираю html в один файл из сегментов
     .pipe( fileInclude() )
+
+    // Меняю пути в итоговом файле, вместо @img от плагина Path Autocomplete, на img/
+    .pipe( app.plugins.replace( /@img\//g, 'img/' ) )
+
+    // Обертка <picture> для webp и оригинального формата
+    .pipe( webpHtmlNosvg() )
 
     // Копируем в папку со сборкой
     .pipe( app.gulp.dest( app.path.build.html ) )
