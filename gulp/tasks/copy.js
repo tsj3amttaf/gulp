f667df7221782a358241da80fa7f37f0d1e7f4ce
tsj3amttaf <uyaskjahhkaskdaodpqwodhjkbz@gmail.com>
2024-10-24 19:49:47 +0700

    save

--- gulp/tasks/copy.js
+++
@@ -1,8 +0,0 @@
-export const copy = () => {
-
-    // Определяем папку с исходниками
-    return app.gulp.src( app.path.src.files )
-
-    // Копируем в папку со сборкой
-    .pipe( app.gulp.dest( app.path.build.files ) )
-}
--- src/js/scripts.js
+++
@@ -1,5 +0,0 @@
-// Импортируем
-import * as flsFunction from './modules/functions.js';
-
-// Проверяем webp
-flsFunction.isWebp();
